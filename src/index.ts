afec39d1bcc8307bf19a577bb057d3747e8ff9f2
Greg <gskril@gmail.com>
2023-03-16 11:31:55 -0400

    Wrap in express app

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,66 @@
 import 'dotenv/config'
+import express from 'express'
 
 import { getProfiles } from './ens.js'
 import { getTopVoters } from './snapshot.js'
 
-const space = 'ens.eth'
-const topVoters = await getTopVoters(space)
-const profiles = await getProfiles(topVoters.addresses)
+const app = express()
+const port = process.env.PORT || 3000
 
-// count number of profiles that have a name
-const profilesWithNames = profiles.filter(
-  (profile) => profile.name !== undefined
-).length
+app.listen(port, () => {
+  console.log(`Your application is running on port ${port}`)
+})
 
-// count number of profiles that have certain text records
-const stats = profiles.reduce((acc: { [key: string]: number }, profile) => {
-  acc['topVoters'] = topVoters.addresses.length
-  acc['profilesWithNames'] = profilesWithNames
+app.get('/', async (req, res) => {
+  const { space } = req.query
 
-  profile.textRecords?.forEach((record) => {
-    const key = Object.keys(record)[0]
-    if (acc[key]) {
-      acc[key]++
-    } else {
-      acc[key] = 1
-    }
-  })
-  return acc
-}, {})
+  if (typeof space !== 'string') {
+    return res.status(400).json({ message: 'Invalid space. Must be a string' })
+  }
 
-const result = {
-  space,
-  profiles,
-  stats,
+  try {
+    const stats = await getStatsForSpace(space)
+    res.status(200).json(stats)
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ message: 'Something went wrong', err })
+  }
+})
+
+async function getStatsForSpace(space: string) {
+  const topVoters = await getTopVoters(space)
+  const profiles = await getProfiles(topVoters.addresses)
+
+  // count number of profiles that have a name
+  const nameStats = profiles.filter(
+    (profile) => profile.name !== undefined
+  ).length
+
+  // count number of profiles that have certain text records
+  const recordsStats = profiles.reduce(
+    (acc: { [key: string]: number }, profile) => {
+      profile.textRecords?.forEach((record) => {
+        const key = Object.keys(record)[0]
+        if (acc[key]) {
+          acc[key]++
+        } else {
+          acc[key] = 1
+        }
+      })
+      return acc
+    },
+    {}
+  )
+
+  const result = {
+    space,
+    stats: {
+      topVoters: topVoters.addresses.length,
+      profilesWithNames: nameStats,
+      records: recordsStats,
+    },
+    profiles,
+  }
+
+  return result
 }
-
-console.log(result)
-
-// TODO: refactor script to work across multiple Snapshot Spaces
