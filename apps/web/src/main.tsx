a4138b74c700b9c171779919d9c0ca30ac34574f
Greg <gskril@gmail.com>
2023-03-17 18:21:28 -0400

    Add thorin

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,16 @@
+import { ThemeProvider } from 'styled-components'
+import { ThorinGlobalStyles, lightTheme } from '@ensdomains/thorin'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+
 import './index.css'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={lightTheme}>
+      <ThorinGlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
---
+++ apps/web/src/theme.ts
@@ -0,0 +1,8 @@
+import 'styled-components'
+import { lightTheme } from '@ensdomains/thorin'
+
+type ExtendedTheme = typeof lightTheme
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends ExtendedTheme {}
+}
