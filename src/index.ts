8a6db85a94a9eb4fdfcbf84d035b98818d9dd1f2
Greg <gskril@gmail.com>
2023-03-16 20:17:42 -0400

    Save a bunch of spaces to cache on start

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,12 @@ import express from 'express'
 import NodeCache from 'node-cache'
 
 import { getStatsForSpace } from './lib'
+import { TOP_SPACES } from './constants'
 
 const app = express()
 const port = process.env.PORT || 3000
+
+const spaces = TOP_SPACES
 const cache = new NodeCache()
 
 app.listen(port, () => console.log(`App is running on port ${port}`))
@@ -19,27 +22,33 @@ app.get('/:space', async (req, res) => {
   const { space } = req.params
   const stats = cache.get(space)
 
-  // If stats are in cache, return them. Otherwise, fetch them and cache them.
   if (stats) {
+    // If stats are in cache, return them
     res.status(200).json(stats)
   } else {
+    // If stats are not in cache, fetch them and add them to cache
     try {
       const stats = await getStatsForSpace(space)
       res.status(200).json(stats)
       cache.set(space, stats)
+      spaces.push(space)
     } catch (err) {
       res.status(400).json({ message: `Error getting space ${space}` })
     }
   }
 })
 
-const spaces = ['ens.eth']
-
 async function updateCache() {
+  const startTime = Date.now()
+
   const stats = await Promise.all(
     spaces.map((space) => getStatsForSpace(space))
   )
   spaces.forEach((space, index) => cache.set(space, stats[index]))
+
+  const endTime = Date.now()
+  const timeTaken = endTime - startTime
+  console.log(`Cache updated in ${timeTaken / 1000} seconds`)
 }
 
 // Seed cache on server start
