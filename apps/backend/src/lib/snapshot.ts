dd2c9d610c8bacf7edeea698c1d3e0f857c69a6d
Greg <gskril@gmail.com>
2023-03-17 22:20:52 -0400

    Use last 6 months of Snapshot votes

--- apps/backend/src/lib/snapshot.ts
+++ apps/backend/src/lib/snapshot.ts
@@ -2,13 +2,21 @@ import { request, gql } from 'graphql-request'
 
 import { SNAPSHOT_HUB_API } from '../constants.js'
 
+function getUnixTimeSixMonthsAgo() {
+  const currentDate = new Date()
+  currentDate.setMonth(currentDate.getMonth() - 6)
+  return Math.floor(currentDate.getTime() / 1000)
+}
+
 export async function getTopVoters(space: string) {
+  const unixTimeSixMonthsAgo = getUnixTimeSixMonthsAgo()
+
   const topVotersQuery = gql`
     query ($spaces: [String]) {
       votes(
         first: 1000
         orderBy: "vp"
-        where: { created_gte: 1662678000, space_in: $spaces }
+        where: { created_gte: unixTimeSixMonthsAgo, space_in: $spaces }
       ) {
         proposal {
           title
